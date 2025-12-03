29c74a92f8e4a4a5a06aa01cc02b9dbaf70d8c05
geethanchanaka <chanakageethan@gmail.com>
2025-12-03 11:26:49 +0530

    filter option

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -7,19 +7,48 @@
     <input type="text" name="title" placeholder="Search by title" 
     value="{{ request('title') }}" class="input h-10"/>
  
+    <input type="hidden" name="filter" value="{{request('filter')}}"/>
+
     <button type="submit" class="btn h-10">Search</button>
     <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
 </form>
 
+
+
+<div class="filter-container mb-4 flex">
+    @php
+        $filters = [
+             '' => 'Latest',
+             'popular_last_month' => 'Popular Last Month',
+             'popular_last_6month' => 'Popular Last 6 Month',
+             'highest_rated_last_month' => 'Highest Rated Last Month',
+             'highest_rated_last_6month' => 'Highest Rated Last 6 Month',
+            ];
+    @endphp
+
+    @foreach ($filters as  $key => $label)
+    <a href="{{ route('books.index',[...request()->query() , 'filter' => $key]) }}" class="{{ request('filter') === $key  || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item'}}">
+        {{ $label }}
+    </a>
+        
+    @endforeach
+
+
+</div>
+
+
  <ul>
     @forelse ( $books as  $book)
-                <li class="mb-4">
+                <li class="mb-4">            
         <div class="book-item">
+
             <div
-            class="flex flex-wrap items-center justify-between">
+            class="flex flex-wrap items-center justify-betw
+            een">
             <div class="w-full flex-grow sm:w-auto">
                 <a href="{{ route('books.show',$book) }}" class="book-title">{{ $book->title }}</a>
                 <span class="book-author">by {{ $book->author }}</span>
+           
             </div>
             <div>
                 <div class="book-rating">
